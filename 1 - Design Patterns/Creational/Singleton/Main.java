542b02ee0aa6bfb2b99a2f5c6fa79fbc94a73fab
Siddhanth Pranav Rao <siddhanthprao@gmail.com>
2025-05-24 11:08:46 +0530

    LLD Practice

--- 1 - Design Patterns/Creational/Singleton/Main.java
+++ 1 - Design Patterns/Creational/Singleton/Main.java
@@ -1,11 +1,25 @@
+class Singleton {
+		private static volatile Singleton uniqueInstance;
+		
+		private Singleton() {}
+		
+		public static Singleton getInstance() {
+			if (uniqueInstance == null) {
+				synchronized (Singleton.class) {
+					if (uniqueInstance == null) {
+						uniqueInstance = new Singleton();
+					}
+				}
+			} 
+			
+			return uniqueInstance;
+		}
+	}
+
 public class Main {
 	
-	class Singleton {
-		
-	}
-	
 	public static void main(String[] args) {
-		
+		Singleton.getInstance();
 		
 	}
 }
